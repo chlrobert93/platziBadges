0cba505beb8c2cd20514ade4cb729f667ac97069
Eder <chlrobert93@gmail.com>
2021-07-26 17:46:46 -0500

    Agregar stilos css

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,27 @@
 import React from 'react';
-
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component{
 
   render() {
     return(
-      <div>
-       <div>
+      <div className="Badge">
+       <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" /> 
         </div> 
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-          <h1>Eder <br/> Kaufman</h1>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/29/1468960520-mewtwo.jpg?resize=480:*" alt="Avatar"/>
+          <h1>Eder <br/> Chavarria</h1>
         </div>
 
-        <div>
-          <p>Front Engineer</p>
-          <p>@sparragus</p>
+        <div className="Badge__section-info">
+          <h3>Front Engineer</h3>
+          <div>@sparragus</div>
         </div>
 
-        <div>
+        <div className="Badge__footer">
           #platziconf
         </div>
       </div>
