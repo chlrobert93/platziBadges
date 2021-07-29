067664bfa006e3d996f41a0f4ca15d2673b41095
Eder <chlrobert93@gmail.com>
2021-07-29 11:38:46 -0500

    Manejo de estados

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,11 +1,28 @@
-import React from 'react';
+ import React from 'react';
 
 class BadgeForm extends React.Component{
+    state = {
+        firstName:"",
+        jobTitle: 'Designer',
+        email:'',
+        lastName:'',
+        twitter: '',
+    };
+
     hanleChange =   e =>{
-      console.log({
+    /*  console.log({
           name: e.target.name,
           value: e.target.value});
-    };
+    };*/
+
+    //Le vamos a pasar un objecto con la información que queremos guardar
+    this.setState({
+        //firstName: e.target.value,s
+        //Lo que va ser es que si es el imput del (name) quien llama esta función
+        //Se va guardar la información dentro de la llave (name)
+        [e.target.name]: e.target.value,
+    });
+};
 
     handleClick = e => {
         console.log("Button was clicked");
@@ -14,9 +31,14 @@ class BadgeForm extends React.Component{
     handleSubmit = e =>{
       e.preventDefault();
       console.log("Form was submitted");
+      //Leer los valores
+      console.log(this.state);
     };
 
-
+    //Convertirlos a controlados 
+    //Va despegar cada uno de los input
+    //This.state ---para leer el stado 
+    //value={this.state.firstName} />    
 
     render(){
         return (
@@ -25,11 +47,59 @@ class BadgeForm extends React.Component{
 
                 <form onSubmit={this.handleSubmit}>
                   <div className="form-group">
-                    <label>First</label>
-                    <input onChange={this.hanleChange} 
-                    className="form-control"
+                    <label>First Name</label>
+                    <input 
+                    onChange={this.hanleChange} 
+                    className="form-control"    
                     type="text"
-                    name="firstName" />
+                    placeholder="Enter your first name"
+                    name="firstName"
+                    value={this.state.firstName} />              
+                  </div>
+
+                  <div className="form-group">
+                      <label>Last Name</label>
+                      <input
+                      onChange={this.hanleChange}
+                      className="form-control"
+                      type="text"
+                      placeholder="Enter your last name"
+                      name="lastName" 
+                      value={this.state.lastName}/>      
+                  </div>
+
+                  <div className="form-group">
+                      <label>Email</label>
+                      <input
+                      onChange={this.hanleChange}
+                      className="form-control"
+                      type="email"
+                      placeholder="Enter your email"
+                      name="email" 
+                      value={this.state.email}/>
+                  </div>
+
+
+                  <div className="form-group">
+                      <label>JobTitle</label>
+                      <input
+                      onChange={this.hanleChange}
+                      className="form-control"
+                      type="text"
+                      placeholder="Enter your jobTitle"
+                      name="jobTitle" 
+                      value={this.state.jobTitle}/>
+                  </div>
+
+                  <div className="form-group">
+                      <label>Twitter</label>
+                      <input
+                      onChange={this.hanleChange}
+                      className="form-control"
+                      type="text"
+                      placeholder="Enter your twitter"
+                      name="twitter" 
+                      value={this.state.twitter}/>
                   </div>
 
                   <button  onClick={this.handleClick} className="btn btn-primary">Save</button>
